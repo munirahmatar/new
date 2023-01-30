04e441e6a309c2cb4e4a9e8b775476caa6b0eaf4
Muneerah Matar <muneerahmatar@Munirahs-MacBook-Air.local>
2023-01-30 17:04:09 +0300

    first commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-2.cgb0ashonb75.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udagram-p
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello-from-other-side
+export URL=http://udagram-api-dev.eba-dk3iqvkm.us-east-1.elasticbeanstalk.com
